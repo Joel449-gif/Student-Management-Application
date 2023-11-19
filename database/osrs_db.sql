bd4209c8decca076f0e68f731b0c02c0bce1d387
Joel Joseph <75429647+Joel449-gif@users.noreply.github.com>
2023-11-19 22:19:37 +0530

    Add files via upload

---
+++ database/osrs_db.sql
@@ -0,0 +1,287 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 19, 2023 at 04:42 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `osrs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int(30) NOT NULL,
+  `level` varchar(200) NOT NULL,
+  `section` varchar(200) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`id`, `level`, `section`, `date_created`) VALUES
+(7, '10', 'A', '2023-11-19 21:02:08'),
+(8, '10', 'B', '2023-11-19 21:02:15'),
+(9, '10', 'C', '2023-11-19 21:02:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `results`
+--
+
+CREATE TABLE `results` (
+  `id` int(30) NOT NULL,
+  `student_id` int(30) NOT NULL,
+  `marks_percentage` varchar(5) NOT NULL,
+  `class_id` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `results`
+--
+
+INSERT INTO `results` (`id`, `student_id`, `marks_percentage`, `class_id`, `date_created`) VALUES
+(1, 1, '87.67', 1, '2020-11-21 16:57:05'),
+(2, 2, '90.33', 1, '2020-11-25 16:45:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result_items`
+--
+
+CREATE TABLE `result_items` (
+  `id` int(30) NOT NULL,
+  `result_id` int(30) NOT NULL,
+  `subject_id` int(30) NOT NULL,
+  `mark` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `result_items`
+--
+
+INSERT INTO `result_items` (`id`, `result_id`, `subject_id`, `mark`, `date_created`) VALUES
+(1, 1, 2, 88, '2020-11-21 16:57:05'),
+(2, 1, 1, 85, '2020-11-21 16:57:05'),
+(3, 1, 3, 90, '2020-11-21 16:57:05'),
+(4, 2, 2, 90, '2020-11-25 16:45:52'),
+(5, 2, 1, 88, '2020-11-25 16:45:52'),
+(6, 2, 3, 93, '2020-11-25 16:45:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(30) NOT NULL,
+  `student_code` varchar(50) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `middlename` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `class_id` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `student_code`, `firstname`, `middlename`, `lastname`, `gender`, `address`, `class_id`, `date_created`) VALUES
+(4, '345678', 'Emily ', '', 'Smith', 'Female', '789 Pine Road, Villagetown, State 67890', 8, '2023-11-19 21:06:46'),
+(5, '567890', 'Jessica ', '', 'Martinez', 'Female', '890 Maple Drive, Boroughburg, State 45678', 9, '2023-11-19 21:07:24'),
+(6, '901234', 'Kevin ', '', 'Davis', 'Male', '234 Cedar Lane, Hamlet City, State 34567', 9, '2023-11-19 21:08:15'),
+(7, '789012', 'Michael ', '', 'Rodriguez', 'Male', '456 Oak Avenue, Townsville, State 12345', 7, '2023-11-19 21:08:55'),
+(8, '123457', 'Sarah ', '', 'Johnson', 'Female', '123 Main Street, Cityville, State 56789', 7, '2023-11-19 21:09:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(30) NOT NULL,
+  `subject_code` varchar(50) NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `subject_code`, `subject`, `description`, `date_created`) VALUES
+(4, '	ST001', 'Maths', 'Mathematics', '2023-11-19 21:03:41'),
+(5, 'ST002', 'Science', 'Science', '2023-11-19 21:04:05'),
+(6, 'ST003', 'Social ', 'Social Science', '2023-11-19 21:04:59'),
+(7, 'ST004', 'English', 'English', '2023-11-19 21:05:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'Student Management Application', 'sma@gmail.com', '99887760', '3B  old Road, Chennai, Tamil Nadu, 600053', '1605927480_download.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` int(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `type`, `date_created`) VALUES
+(1, 'Administrator', '', 'admin', '0192023a7bbd73250516f069df18b500', 1, '2020-11-20 13:25:41');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `results`
+--
+ALTER TABLE `results`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `result_items`
+--
+ALTER TABLE `result_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `results`
+--
+ALTER TABLE `results`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `result_items`
+--
+ALTER TABLE `result_items`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
